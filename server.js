6fd9ac867932ed03afd43f4e0d8bf341b00becac
Richard Allison <richard.allison.18@outlook.com>
2018-02-28 16:05:55 +0000

    Added server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
-const app = express();
-const path = require('path');
+const parser = require('body-parser');
+const server = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+server.use(express.static('client/build'));
+
+server.listen(3000, function () {
+  console.log('App running on port '+ this.address().port);
+});
